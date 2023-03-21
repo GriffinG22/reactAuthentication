7368c97d54877155236e67d4940ed784415da2df
Griffin Gradke <griffin.gradke@gmail.com>
2023-03-21 14:39:59 -0500

    auto logout after 1hr

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,21 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'})
+    }, 3600000);
+  }, [token, submit])
 
   return (
     <>
